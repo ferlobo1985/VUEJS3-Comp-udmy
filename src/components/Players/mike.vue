8d820ff10fbb3bf45b845a0a9a8cc944f119f69f
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-09-29 01:36:03 -0300

    17-Keep alive

--- src/components/Players/mike.vue
+++ src/components/Players/mike.vue
@@ -1,3 +1,18 @@
 <template>
     <h3>Mike is on</h3>
 </template>
+
+<script>
+    export default {
+        name:'mike',
+        activated(){
+            console.log('hello mike!!')
+        },
+        deactivated(){
+            console.log('by mike!!')
+        },
+        unmounted(){
+            console.log('Mike unmounted')
+        }
+    }
+</script>
