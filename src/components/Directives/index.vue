eb49480388655f83f32ea2e5f213209788f1e4d8
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-09-29 02:50:09 -0300

    22-Local custom directives

--- src/components/Directives/index.vue
+++ src/components/Directives/index.vue
@@ -1,4 +1,6 @@
 <template>
+
+    <div v-user-lastname="lastname"></div>
     <div v-awesome.red.big="textToRender"></div>
 
     <button @click="this.textToRender = 'Something else'">
@@ -11,7 +13,15 @@
 export default {
     data(){
         return{
-            textToRender:'Text from data'
+            textToRender:'Text from data',
+            lastname:'Jones'
+        }
+    },
+    directives:{
+        'user-lastname':{
+            beforeMount(el,binding){
+                el.innerHTML =  binding.value
+            }
         }
     }
 }    
